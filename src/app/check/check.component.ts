eafc7fb514b53d0076c4f4f89da8397b821dbd8d
rafaScalet <rafaelscalet2018@gmail.com>
2024-03-23 09:56:30 -0300

    initial configuration, create the 'check' component

---
+++ src/app/check/check.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-check',
+  templateUrl: './check.component.html',
+  styleUrl: './check.component.css'
+})
+export class CheckComponent {
+
+}
