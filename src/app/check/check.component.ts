1000c260b3c3202b4ccbcbe297ec8e209cf54b61
rafaScalet <rafaelscalet2018@gmail.com>
2024-03-23 10:42:18 -0300

    added 'order' function and improvements on 'set' function

--- src/app/check/check.component.ts
+++ src/app/check/check.component.ts
@@ -3,14 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-check',
   templateUrl: './check.component.html',
-  styleUrl: './check.component.css'
+  styleUrl: './check.component.css',
 })
 export class CheckComponent {
-  numbers: number[] = []
-  aux: any
+  numbers: number[] = [];
+  choice: boolean = false
+  aux: any;
 
-  set(){
-    this.numbers.push(this.aux)
-    this.aux = ''
+  set() {
+    if (!this.numbers.includes(this.aux)) {
+      if (!(this.aux == null)) {
+        this.numbers.push(this.aux);
+        this.aux = '';
+      } else {
+        alert('insira um número');
+      }
+    }else{
+      alert('este número já foi inserido')
+      this.aux = ''
+    }
+  }
+
+  order(choice: string = '') {
+    this.numbers.sort((a, b) => eval(choice));
   }
 }
